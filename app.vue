b218683c4fa677a29543984e9b1e4e8c235cfb55
渡邉英資 <awatanabe@fminc.xsrv.jp>
2022-12-03 15:44:19 +0900

    ignore .idea

--- app.vue
+++ app.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
     <button class="text-red-400">hello World</button>
-    <div>aaaa</div>
+    <ul class="mt-4 space-y-2">
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+    </ul>
   </div>
 </template>
 
+<script lang="ts">
+</script>
+
+<style lang="scss" scoped>
+</style>
