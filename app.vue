4b6418c423d6b7b79b3d226d03f1705b6b98ccb3
渡邉英資 <awatanabe@fminc.xsrv.jp>
2022-12-05 21:29:59 +0900

    fix style

--- app.vue
+++ app.vue
@@ -1,16 +1,10 @@
 <template>
-  <div>
-    <button class="text-red-400">hello World</button>
-    <ul class="mt-4 space-y-2">
-      <li>HTML</li>
+  <div class="m-8 p-8 bg-gray-200 shadow-lg">
+    <h1 class="font-bold text-red-400 text-4xl w-full">hello World</h1>
+    <ul class="mt-4 space-y-2 list-decimal hover:text-red-500">
+      <li class="">HTML</li>
       <li>CSS</li>
       <li>JavaScript</li>
     </ul>
   </div>
 </template>
-
-<script lang="ts">
-</script>
-
-<style lang="scss" scoped>
-</style>
